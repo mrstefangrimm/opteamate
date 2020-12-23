12dea0b2366b893bb40ff2bf9cd1e590726448c9
mrstefangrimm <motionsystems@grimm.com>
2020-12-23 18:14:32 +0100

    Rename folders to 'Payloads' and 'Resposes'

--- OpteaMate.Domain/Payloads/OptimumData.cs
+++ OpteaMate.Domain/Payloads/OptimumData.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) 2020 Stefan Grimm. All rights reserved.
 // Licensed under the GPL. See LICENSE file in the project root for full license information.
 //
-
 using System.ComponentModel.DataAnnotations;
 
 namespace OpteaMate.Domain {
